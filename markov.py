00c0bee2ffb43b6c55ffec88f969b7e750d7d998
Kaytee Cannon <kaytee.cannon@me.com>
2020-10-07 21:59:42 +0000

    End of day; bot responding with incorrect output

--- markov.py
+++ markov.py
@@ -2,6 +2,8 @@
 
 import sys
 from random import choice
+import discord
+import os
 
 
 def open_and_read_file(filenames):
@@ -64,3 +66,23 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+print(chains)
+
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send(chains)
+
+
+client.run(DISCORD_TOKEN)
